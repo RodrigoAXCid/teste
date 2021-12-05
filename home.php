c4c5bda5dcf52c13dd05fbb0528032cfd3f3c14c
RodrigoAXCid <50101721+RodrigoAXCid@users.noreply.github.com>
2021-12-05 21:05:16 +0000

    commit

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <h1>Olá Mundo</h1>
+    <?php 
+        $COLOR = "red";
+        $color = 10;
+        $coLOR = 9.9;
+        echo "My car is " . $color . "<br>";
+        echo "My house is " . $COLOR . "<br>";
+        echo "My boat is " . $coLOR . "<br>";
+    ?>
+    <?php
+        echo "<h2>PHP is Fun!</h2>";
+        echo "Hello world!<br>";
+        echo "I'm about to learn PHP!<br>";
+        echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+    ?>
+</html>
---
+++ teste.php
@@ -0,0 +1,1 @@
+hello World eu sou o carlos
